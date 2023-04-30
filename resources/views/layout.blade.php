3781fa4dba7f126bd660390d6a499634bd6f4e1d
Youssef-Ayoub <81822018+Youssef-Ayoub@users.noreply.github.com>
2023-05-01 00:45:32 +0200

    update dashboard

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Blank Page</title>
+    <title>Education system</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
@@ -184,24 +184,12 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="../../index3.html" class="brand-link">
-                <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
-                    class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
+
+                <span class="brand-text font-weight-light">Education System</span>
             </a>
 
             <!-- Sidebar -->
             <div class="sidebar">
-                <!-- Sidebar user (optional) -->
-                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div class="image">
-                        <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-                    </div>
-                    <div class="info">
-                        <a href="#" class="d-block">Alexander Pierce</a>
-                    </div>
-                </div>
-
-
 
                 <!-- Sidebar Menu -->
                 <nav class="mt-2">
@@ -210,7 +198,7 @@
                         <!-- Add icons to the links using the .nav-icon class
                         with font-awesome or any other icon font library -->
                         <li class="nav-item menu-open">
-                            <a href="#" class="nav-link active">
+                            <a href="#" class="nav-link @if (Request::segment(2) == 'category') active @endif ">
                                 <i class="	fa fa-braille"></i>
                                 <p>
                                     Category
@@ -234,7 +222,7 @@
                         </li>
                         {{-- start of couurse --}}
                         <li class="nav-item menu-open">
-                            <a href="#" class="nav-link">
+                            <a href="#" class="nav-link  @if (Request::segment(2) == 'course') active @endif ">
                                 <i class="fa fa-graduation-cap"></i>
                                 <p>
                                     Courses
@@ -256,6 +244,8 @@
                                 </li>
                             </ul>
                         </li>
+
+
 
                         <li class="nav-item">
                             <a href="{{ url('/logout') }}" class="nav-link">
