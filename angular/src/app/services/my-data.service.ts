dfd04b99de648a6bb996372f2b92e0d82bf68abf
Ahmed Badr Shabaan Ahmed <59667579+AhmedBadrShaban@users.noreply.github.com>
2023-06-25 17:46:22 +0300

    Update my-data.service.ts
    ss

--- angular/src/app/services/my-data.service.ts
+++ angular/src/app/services/my-data.service.ts
@@ -18,15 +18,14 @@ export class MyDataService {
    return this.http.post<any>('http://localhost/CoursePilot/Education-system-laravel/laravel%20finale/public/api/login', loginData)
   }
   getAllCategories(){
-    const headers = { 'Authorization': 'Bearer 8|25HoPY3ItEA611U3saxIzK1RwV4lo9zhyjeB9HF8', 'Accept':'application/vnd.api+json' , 'Content-Type': 'application/vnd.api+json'
+    const headers = { 'Accept':'application/vnd.api+json' , 'Content-Type': 'application/vnd.api+json'
   }
      return this.http.get<any>('http://localhost/CoursePilot/Education-system-laravel/laravel%20finale/public/api/categories' ,{headers})
   }
   AllCourses(){
-      const headers = { 'Authorization': 'Bearer 8|25HoPY3ItEA611U3saxIzK1RwV4lo9zhyjeB9HF8' ,'Accept':'application/vnd.api+json' , 'Content-Type': 'application/vnd.api+json' }
+      const headers = { 'Accept':'application/vnd.api+json' , 'Content-Type': 'application/vnd.api+json' }
       console.log(headers);
     return this.http.get<any>('http://localhost/Education-system-laravel/laravel%20test/public/api/courses' , {headers} )
-
   }
   AllComments(){
     return this.http.get<any>('http://localhost/CoursePilot/Education-system-laravel/laravel%20finale/public/api/comments/course/3' )
